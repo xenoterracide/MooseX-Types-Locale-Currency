a610f4bf2e0b5b06b517836d0c09bd57e5375291
Caleb Cushing <xenoterracide@gmail.com>
2011-08-26 22:49:12 -0500

    add first basic implementation
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/MooseX/Types/Locale/Currency.pm
+++ lib/MooseX/Types/Locale/Currency.pm
@@ -1,10 +1,19 @@
 package MooseX::Types::Locale::Currency;
-use 5.006;
+use 5.008;
 use strict;
 use warnings;
-BEGIN {
-	# VERSION
-}
+
+# VERSION
+
+use MooseX::Types -declare => [ qw( AlphaCurrencyCode ) ];
+use MooseX::Types::Moose qw( Str );
+use namespace::autoclean;
+
+use Locale::Currency;
+
+enum AlphaCurrencyCode,
+	[ all_currency_codes ]
+	;
 
 1;
 
