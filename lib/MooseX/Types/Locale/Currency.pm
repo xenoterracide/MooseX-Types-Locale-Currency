3e2db1b7b2020be3266421376a2ba4172e92a2ac
Caleb Cushing <xenoterracide@gmail.com>
2011-08-31 01:02:02 -0500

    initial implementation and docs
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/MooseX/Types/Locale/Currency.pm
+++ lib/MooseX/Types/Locale/Currency.pm
@@ -5,16 +5,79 @@ use warnings;
 
 # VERSION
 
-use MooseX::Types -declare => [ qw( AlphaCurrencyCode ) ];
-use MooseX::Types::Moose qw( Str );
+use MooseX::Types -declare => [ qw( CurrencyCode ) ];
+use MooseX::Types::Moose qw( Str Int );
 use namespace::autoclean;
 
 use Locale::Currency;
 
-enum AlphaCurrencyCode,
+enum CurrencyCode,
 	[ all_currency_codes ]
+	;
+
+coerce CurrencyCode,
+	from Int,
+	via {
+		currency_code2code( $_, 'num', 'alpha' )
+	}
+	;
+
+coerce CurrencyCode,
+	from Str,
+	via {
+			if ( length($_) == 3 && $_ =~ /[[:alpha:]]*/ ) {
+				lc $_;
+			}
+		}
 	;
 
 1;
 
-# ABSTRACT: MooseX::Types::Locale::Currency
+# ABSTRACT: Moose Types related to Locale Currency
+
+=head1 SYNOPSIS
+
+	{
+		package My::Object;
+		use Moose;
+		use MooseX::Types::Locale::Currency qw( CurrencyCode );
+
+		has currency_code => (
+			is => 'ro',
+			isa => CurrencyCode,
+		);
+		__PACKAGE__->meta->make_immutable;
+	}
+
+	my $obj = My::Object->new({
+		currency_code => 'usd'
+	});
+
+=head1 DESCRIPTION
+
+This module provides the C<CurrencyCode> type. The API is not stable, currency
+code may become an object or gain "handles" in order to  support accessing the
+different ways to "view" a currency code.
+
+=head1 TYPES
+
+=over
+
+=item * C<CurrencyCode>
+
+Base Type: C<enum>
+
+will validate the the 3 character string passed to it is in the list of
+C<all_currency_codes>
+
+=back
+
+=head1 SEE ALSO
+
+=over
+
+=item * L<Locale::Currency>
+
+=back
+
+=cut
